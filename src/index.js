17c3e4886b010b2479fb7d4fc7af834844b56bbc
John Gamboa <jayjeex@gmail.com>
2021-07-03 23:23:02 +0800

    july 3, 23:22

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 'use strict';
-
 import './style.css';
 
 console.log('yep, it run!!!');
+
+
+// import Hero  from './img/hero.jpeg';
+// function heroImg(){
+//     const element = document.createElement('div');
+//     element.classList.add('hero');
+//     const imgHero = new Image();
+//     imgHero.src = Hero;
+//     element.appendChild(imgHero);
+//     return element;
+// }
+// const content = document.querySelector('#content');
+// content.insertBefore(heroImg(), content.firstChild);
+
+
+// import ButteredSalmon from './img/foods/buttered-salmon.png'
+// function component(){
+//     const butteredSalmon = new Image();
+//     butteredSalmon.src = ButteredSalmon; 
+//     return butteredSalmon;
+// }
+// const content = document.querySelector('#content > div > div');
+// content.insertBefore(component(), content.firstChild);
+
+// import Location from './img/loc.png';
+// function contact(){
+//     const location = new Image();
+//     location.src = Location;
+//     return location;
+// }
+
+// const content = document.querySelector('#contact');
+// content.appendChild(contact());
